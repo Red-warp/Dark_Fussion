17b85b90e4e66e154fc03c397ad8ffc0b00bfa36
Red warp <88662082+TeamFussion@users.noreply.github.com>
2021-09-12 08:15:31 -0700

    Update lightningrun.py

--- lightningrun.py
+++ lightningrun.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/KeinShin/Black-Lightning /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
+os.system("git clone https://github.com/TeamFussion/Dark_Fussion /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
