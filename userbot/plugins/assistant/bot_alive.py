37186f2b5016c5a71ab2d2b1cfb9dc3ad9af04cc
Red warp <88662082+TeamFussion@users.noreply.github.com>
2021-09-21 09:23:41 -0700

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -9,7 +9,7 @@ from userbot import ALIVE_NAME, bot
 currentversion = "2.1"
 
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Black Lightning"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Đ₳Ɽ₭ Ƒմʂʂìօղ"
 ASSIS_PIC = os.environ.get("ASSIS_PIC", None)
 if ASSIS_PIC is None:
      PM_IMG = "https://telegra.ph/file/beb843ce962a738c52cad.jpg"
@@ -17,15 +17,16 @@ else:
      PM_IMG = ASSIS_PIC
 
 
-pm_caption = " ►**ASSISTANT IS:** `ONLINE`\n\n"
+pm_caption = " ►**Đ₳Ɽ₭ Ƒմʂʂìօղ IS ONLINE**\n\n"
 pm_caption += "► **SYSTEMS STATS**\n"
 pm_caption += "► **Telethon Version:** `1.15.0` \n"
 pm_caption += f"► **Assistant Version** : `{currentversion}`\n"
 pm_caption += f"► **My Master** : {DEFAULTUSER} \n"
-pm_caption += "► **License** : [GNU General Public License v3.0](https://github.com/KeinShin/Black-Lightning/blob/master/LICENSE)\n"
-pm_caption += "► **Copyright** : [Black Lighting](GitHub.com/keinshin/Black-Lightning)\n"
-light = [[Button.url("✧Repos✧", "https://github.com/KeinShin/Black-Lightning"), Button.url("✧Support✧", "https://t.me/lightning_support_group")]]
-light +=[[custom.Button.inline("✧Help✧", data="gibcmd")]]
+pm_caption += "► **License** : [General Public License](https://github.com/TeamFussion/Dark_Fussion/blob/master/LICENSE)\n"
+pm_caption += "► **Copyright** : [Đ₳Ɽ₭ Ƒմʂʂìօղ](GitHub.com/TeamFussion/Dark_Fussion)\n"
+light = [[Button.url("✧Repo✧", "https://github.com/TeamFussion/DarkFussion"), Button.url("✧Deploy✧", "https://heroku.com/deploy?template=https://github.com/TeamFussion/Dark_Fussion")]]
+light +=[[Button.url("✧Channel✧", "https://t.me/DarkFussion") , Button.url("✧Group✧", "https://t.me/Dark_Fussion_chat")]]
+light +=[[Button.url("✧Devoloprer✧" , "https://github.com/TeamFussion")]]
 @tgbot.on(events.NewMessage(pattern="^/alive" , func=lambda e: e.sender_id == bot.uid))
 async def _(event):
     await tgbot.send_file(event.chat_id, PM_IMG, caption=pm_caption, buttons=light)
