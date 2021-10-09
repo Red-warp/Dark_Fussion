7b8080e5353c73d524570cc990254537584ef9c3
Red warp <88662082+TeamFussion@users.noreply.github.com>
2021-10-09 15:27:35 +0530

    Update _inlineee.py

--- userbot/plugins/_inlineee.py
+++ userbot/plugins/_inlineee.py
@@ -280,11 +280,11 @@ async def lightning_pugins_query_hndlr(lightning):
     if light_pulu_name in CMD_HELP.keys():
        
        lightning_help_strin  = f"**✘ Commands Available ✘**\n" 
-       lightning_help_strin  += f"**✘ Name ✘ :** `{light_pulu_name}` \n{CMD_HELP[light_pulu_name]}"
+       lightning_help_strin  += f"**✘ Name ✘ :** `{light_pulu_name}` \n{CMD_HELPlight_pulu_name}"
        lightning_is_best = lightning_help_strin 
     
     else:
-       lightning_help_strin  = f"**✘ Commands Available ✘\n✘ Name ✘ : ** `{light_pulu_name}` \n`{CMD_LIST[light_pulu_name]}`"
+       lightning_help_strin  = f"**✘ Commands Available ✘\n✘ Name ✘ : ** `{light_pulu_name}` \n`{CMD_LISTlight_pulu_name}`"
        lightning_is_best = lightning_help_strin 
     if len(lightning_is_best) >= 4096:
           keinshin = "`Wait.( ͡🔥 ͜ʖ ͡🔥)`"
