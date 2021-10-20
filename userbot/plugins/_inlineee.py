82d07d8466295ed9c64fed4fa6069d6201d9bf8c
Red warp <88662082+TeamFussion@users.noreply.github.com>
2021-10-20 19:55:37 +0530

    Update _inlineee.py

--- userbot/plugins/_inlineee.py
+++ userbot/plugins/_inlineee.py
@@ -537,13 +537,13 @@ async def lmaao(lightning):
         fukoff = "You Don't belong to my master's category. So, why should i follow your orders\nHence, Fuck off" 
         await lightning.answer(fukoff, alert=True)
 
-@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"sett"))) 
-async def lmaao(lightning):
-    if lightning.query.user_id == bot.uid:
-        await lightning.delete()
-   else:
-        fukoff = "You Don't belong to my master's category. So, why should i follow your orders\nHence, Fuck off" 
-        await lightning.answer(fukoff, alert=True)
+#@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"sett"))) 
+#async def lmaao(lightning):
+   # if lightning.query.user_id == bot.uid:
+      #  await lightning.delete()
+  # else:
+      #  fukoff = "You Don't belong to my master's category. So, why should i follow your orders\nHence, Fuck off" 
+      #  await lightning.answer(fukoff, alert=True)
 
 import requests
 
