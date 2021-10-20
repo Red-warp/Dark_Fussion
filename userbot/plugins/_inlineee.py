46616ae87d5bbe462612c5239d1d753534bff08a
Red warp <88662082+TeamFussion@users.noreply.github.com>
2021-10-20 22:41:37 +0530

    Update _inlineee.py

--- userbot/plugins/_inlineee.py
+++ userbot/plugins/_inlineee.py
@@ -176,7 +176,7 @@ else:
 LIGHTNING_BOT_PIC = (
     LIGHTNING_BOT_PIC
     if LIGHTNING_BOT_PIC
-    else "https://telegra.ph/file/ff90ed0b44221a7b438b7.jpg"
+    else "https://te.legra.ph/file/d4a96b95ef63eed7d97a0.jpg"
 )
 
 
@@ -204,7 +204,7 @@ async def inline_handler(lightning):
         await lightning.answer([result] if result else None)
     elif lightning.query.user_id == bot.uid and query.startswith("**Black") or query.startswith("Black"):
         result = builder.photo(
-            file="https://te.legra.ph/file/c284c2c263795753516b9.jpg",
+            file="https://te.legra.ph/file/7764974550122172ef810.jpg",
             text=f"** Bᴏᴛ Oғ {LIGHTNINGUSER}\n\n Mᴀɪɴ Mᴇɴᴜ\n Pʟᴜɢɪɴs ~ {len(CMD_LIST)}\n\n Ⲃⲟⲧ Ⲓⲛϝⲟʀⲙⲁⲧⲓⲟⲛ\n╭━━━━━━━━━━━━━━━➣\n┣⪼ Ⲟⲱⲛⲉʀ - {LIGHTNINGUSER}\n┣⪼ Ⲃⲟⲧ Ⳳⲉʀⲋⲓⲟⲛ - 1.2.7\n┣⪼ Ⳙⲣⲧⲓⲙⲉ - 2m.42s\n┣⪼ Ⲣⲩⲧⲏⲟⲛ - 3.9.96\n┣⪼ Ⲧⲉⳑⲉⲧⲏⲟⲛ - 1.23.0\n╰━━━━━━━━━━━━━━━➣** ",
             buttons=[
                 [custom.Button.inline("✘ Plugins ✘", data="what?"),
