cf12ab0054f14654cafa587601bb1cc389230cff
Red warp <88662082+TeamFussion@users.noreply.github.com>
2021-11-03 18:24:08 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -11,6 +11,8 @@ from userbot import CMD_HNDLR, bot
 from userbot.Config import Var
 from userbot.thunderconfig import Config
 from userbot.utils import load_assistant, load_module, start_assistant
+from userbot.version import __hell__ as hellver
+from telethon.tl.functions.channels import InviteToChannelRequest, JoinChannelRequest
 
 TELE = Var.PRIVATE_GROUP_ID
 BOTNAME = Var.TG_BOT_USER_NAME_BF_HER
