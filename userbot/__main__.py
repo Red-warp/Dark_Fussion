1b18d75ecd3428d24ba88ebc6acd2ae8c32b8545
Akki ThePro <73880884+Akshat7678@users.noreply.github.com>
2021-11-06 10:04:51 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -99,14 +99,4 @@ else:
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
-    bot.run_until_disconnected()               
-                
-                
-               
-                
-                
-                
-               
-                
-                
-
+    bot.run_until_disconnected()   
