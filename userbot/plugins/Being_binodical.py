2c674507c320811316e0201e09895251cb0deac3
Red warp <88662082+TeamFussion@users.noreply.github.com>
2021-11-08 14:19:05 +0530

    Update Being_binodical.py

--- userbot/plugins/Being_binodical.py
+++ userbot/plugins/Being_binodical.py
@@ -11,3 +11,31 @@ from telethon import events
 
 
 @borg.on(events.NewMessage(pattern=r"\.binod", outgoing=True))
+async def bid(event):
+    giveVar = event.text
+    bid = giveVar[4:5]
+    if not bid:
+        bid = "😂"
+    await event.edit(
+        f"{bid}{bid}{bid}{bid}{bid}{bid}{bid}\n{bid}{bid}{bid}{bid}{bid}{bid}{bid}{bid}\n{bid}{bid}                     {bid}{bid}\n{bid}{bid}                     {bid}{bid}\n{bid}{bid}{bid}{bid}{bid}{bid}{bid}{bid}\n{bid}{bid}{bid}{bid}{bid}{bid}{bid}{bid}\n{bid}{bid}{bid}{bid}{bid}{bid}{bid}{bid}\n{bid}{bid}                     {bid}{bid}\n{bid}{bid}                     {bid}{bid}\n{bid}{bid}{bid}{bid}{bid}{bid}{bid}{bid}\n{bid}{bid}{bid}{bid}{bid}{bid}{bid}\n\n{bid}{bid}{bid}{bid}{bid}{bid}\n{bid}{bid}{bid}{bid}{bid}{bid}\n          {bid}{bid}\n          {bid}{bid}\n          {bid}{bid}\n          {bid}{bid}\n          {bid}{bid}\n          {bid}{bid}\n{bid}{bid}{bid}{bid}{bid}{bid}\n{bid}{bid}{bid}{bid}{bid}{bid}\n\n{bid}{bid}                           {bid}{bid}\n{bid}{bid}{bid}                       {bid}{bid}\n{bid}{bid}{bid}{bid}                 {bid}{bid}\n{bid}{bid}  {bid}{bid}               {bid}{bid}\n{bid}{bid}     {bid}{bid}            {bid}{bid}\n{bid}{bid}         {bid}{bid}        {bid}{bid}\n{bid}{bid}             {bid}{bid}    {bid}{bid}\n{bid}{bid}                 {bid}{bid}{bid}{bid}\n{bid}{bid}                     {bid}{bid}{bid}\n{bid}{bid}                          {bid}{bid}\n\n           {bid}{bid}{bid}{bid}{bid}\n     {bid}{bid}{bid}{bid}{bid}{bid}{bid}\n   {bid}{bid}                   {bid}{bid}\n {bid}{bid}                       {bid}{bid}\n{bid}{bid}                         {bid}{bid}\n{bid}{bid}                         {bid}{bid}\n {bid}{bid}                       {bid}{bid}\n   {bid}{bid}                   {bid}{bid}\n      {bid}{bid}{bid}{bid}{bid}{bid}{bid}\n            {bid}{bid}{bid}{bid}{bid}\n\n{bid}{bid}{bid}{bid}{bid}{bid}{bid}\n{bid}{bid}{bid}{bid}{bid}{bid}{bid}{bid}\n{bid}{bid}                      {bid}{bid}\n{bid}{bid}                         {bid}{bid}\n{bid}{bid}                         {bid}{bid}\n{bid}{bid}                         {bid}{bid}\n{bid}{bid}                         {bid}{bid}\n{bid}{bid}                      {bid}{bid}\n{bid}{bid}{bid}{bid}{bid}{bid}{bid}{bid}\n{bid}{bid}{bid}{bid}{bid}{bid}{bid}"
+    )
+    
+    
+    
+    
+CMD_HELP.update(
+    {
+         "alive": "✘** Cmds** ✘: `5`\
+        \n\n✘ **Cmd: ** `.alive`\
+        \n➥ **Info: ** __A kind of showing bot details__\
+        \n\n✘ **Cmd: ** `.falive`\
+        \n➥ **Info: ** __A kind of showing bot details__\
+        \n\n✘ **Cmd : ** `.halive`\
+       \n➥ **Info: ** __A kind of showing bot details__\
+        \n\n✘ **Cmd: ** `.valive`\
+       \n➥ **Info: ** __A kind of showing bot details__\
+       \n\n✘ **Cmd: ** `.dalive`\
+       \n➥ **Info: ** __A kind of showing bot details__\
+       \n\n👩‍💻 **Usage : __To check userbot if it is alive.__**"
+    }
+)
