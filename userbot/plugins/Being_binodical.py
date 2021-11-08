367dbc96799065a724df542e953646cb1cb8726f
Red warp <88662082+TeamFussion@users.noreply.github.com>
2021-11-08 14:17:54 +0530

    Create Being_binodical.py

---
+++ userbot/plugins/Being_binodical.py
@@ -0,0 +1,13 @@
+
+"""
+Say something interesting...
+Syntax: .belo
+    by @Deonnn
+Quotes credits: Being logical Channel
+"""
+
+
+from telethon import events
+
+
+@borg.on(events.NewMessage(pattern=r"\.binod", outgoing=True))
