12a47a52233ec5eac53763a9dc22de8bc0ca68ae
Red warp <88662082+TeamFussion@users.noreply.github.com>
2021-11-11 10:33:00 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -20,35 +20,35 @@ sed = logging.getLogger("Dark Fussion")
 fusion_pic = "https://te.legra.ph/file/56615a80e56dcca9dcfa0.jpg"
 
 #async def add_bot(bot_token):
-  #  await bot.start(bot_token)
-  #  bot.me = await bot.get_me()
-    #bot.uid = telethon.utils.get_peer_id(bot.me)
+    await bot.start(bot_token)
+    bot.me = await bot.get_me()
+    bot.uid = telethon.utils.get_peer_id(bot.me)
 
 #async def startup_log_all_done():
-  #  try:
-    #    await bot.send_message(
-    #        TELE,
-     #       f"**Đ₳Ɽ₭ Ƒմʂʂìօղ has been deployed.\nSend** `{CMD_HNDLR}alive` **to see if the bot is working.\n\nAdd** @{BOTNAME} **to this group and make it admin for enabling all the features of userbot**",
-     #   )
-    #except BaseException:
-    #    print("Either PRIVATE_GROUP_ID is wrong or you have left the group.")
+    try:
+        await bot.send_message(
+            TELE,
+           f"**Đ₳Ɽ₭ Ƒմʂʂìօղ has been deployed.\nSend** `{CMD_HNDLR}alive` **to see if the bot is working.\n\nAdd** @{BOTNAME} **to this group and make it admin for enabling all the features of userbot**",
+        )
+    except BaseException:
+        print("Either PRIVATE_GROUP_ID is wrong or you have left the group.")
 
 
-#if len(argv) not in (1, 3, 4):
-   # bot.disconnect()
-#else:
-    #bot.tgbot = None
-   # if Var.TG_BOT_USER_NAME_BF_HER is not None:
-      #  print("Initiating Inline Bot")
-      #  bot.tgbot = TelegramClient(
-      #      "TG_BOT_TOKEN", api_id=Var.APP_ID, api_hash=Var.API_HASH
-      #  ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
-      #  print("Initialisation finished, no errors")
-       # print("Starting Userbot")
-     #  bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
-       # print("Startup Completed")
-   # else:
-    #    bot.start()
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot = None
+    if Var.TG_BOT_USER_NAME_BF_HER is not None:
+        print("Initiating Inline Bot")
+       bot.tgbot = TelegramClient(
+            "TG_BOT_TOKEN", api_id=Var.APP_ID, api_hash=Var.API_HASH
+        ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
+        print("Initialisation finished, no errors")
+       print("Starting Userbot")
+       bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        print("Startup Completed")
+    else:
+       bot.start()
 
 path = "userbot/plugins/*.py"
 files = glob.glob(path)
