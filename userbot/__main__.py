61c9751732d9770b23e5bfbead9af0a0e4053797
Red warp <88662082+TeamFussion@users.noreply.github.com>
2021-11-15 17:55:47 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -14,7 +14,6 @@ from userbot.utils import load_assistant, load_module, start_assistant
 from telethon.tl.functions.channels import InviteToChannelRequest, JoinChannelRequest
 from telethon.tl import functions
 
-bot_token = Var.TG_BOT_TOKEN_BF_HER
 TELE = Var.PRIVATE_GROUP_ID
 BOTNAME = Var.TG_BOT_USER_NAME_BF_HER
 LOAD_MYBOT = Var.LOAD_MYBOT
@@ -29,9 +28,13 @@ async def add_bot(bot_token):
 
 async def startup_log_all_done():
     try:
-        await bot.send_message(TELE, f"ThunderUserbot has been started")
+        await bot.send_message(
+            TELE,
+            f"Đ₳Ɽ₭ Ƒմʂʂìօղ has been deployed.\nSend {CMD_HNDLR}alive to see if the bot is working.\n\nAdd @{BOTNAME} to this group and make it admin for enabling all the features of userbot",
+        )
     except BaseException:
         print("Either PRIVATE_GROUP_ID is wrong or you have left the group.")
+
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
@@ -39,10 +42,9 @@ else:
     bot.tgbot = None
     if Var.TG_BOT_USER_NAME_BF_HER is not None:
         print("Initiating Inline Bot")
+        # ForTheGreatrerGood of beautification
         bot.tgbot = TelegramClient(
-            "TG_BOT_TOKEN",
-            api_id=Var.APP_ID,
-            api_hash=Var.API_HASH
+            "TG_BOT_TOKEN", api_id=Var.APP_ID, api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
         print("Initialisation finished, no errors")
         print("Starting Userbot")
@@ -51,37 +53,51 @@ else:
     else:
         bot.start()
 
-path = 'userbot/plugins/*.py'
+
+path = "userbot/plugins/*.py"
 files = glob.glob(path)
 for name in files:
     with open(name) as f:
         path1 = Path(f.name)
         shortname = path1.stem
-        load_module(shortname.replace(".py", ""))
+        try:
+            
+         load_module(shortname.replace(".py", ""))
+        except Exception:
+            pass
+print("Đ₳Ɽ₭ Ƒմʂʂìօղ has been deployed! ")
 
-print("Thunderuserbot has been deployed! ")
+print("Setting up Đ₳Ɽ₭ Ƒմʂʂìօղ")
 
-print("Setting up Assisting Bot")
-path = "userbot/plugins/assistant/*.py"
-files = glob.glob(path)
-for name in files:
-    with open(name) as f:
-        path1 = Path(f.name)
-        shortname = path1.stem
-        start_mybot(shortname.replace(".py", ""))
 
-if LOAD_MYBOT == "True":
+if Config.ENABLE_ASSISTANTBOT == "ENABLE":
     path = "userbot/plugins/assistant/*.py"
     files = glob.glob(path)
     for name in files:
         with open(name) as f:
             path1 = Path(f.name)
             shortname = path1.stem
-            load_pmbot(shortname.replace(".py", ""))
-    print("Assisting Bot set up completely!")
-
-print("Thunderuserbot has been started without any issue! Please visit @thunderuserbot on telegram for more")
-bot.loop.run_until_complete(startup_log_all_done())
+            try:
+                
+            
+             load_assistant(shortname.replace(".py", ""))
+            except Exception:
+                pass
+    sed.info("Đ₳Ɽ₭ Ƒմʂʂìօղ Has Been Deployed Successfully !")
+    sed.info("╔════❰ Ⲃⲟⲧ Ⲓⲛϝⲟʀⲙⲁⲧⲓⲟⲛ ❱═❍⊱❁۪۪")
+    sed.info("║┣⪼ Ⲟⲱⲛⲉʀ - Ƒմʂʂìօղ ᴜꜱᴇʀ ")
+    sed.info("║┣⪼ Ⲋⲧⲁⲧυⲋ - Ⲟⲛⳑⲓⲛⲉ")
+    sed.info("║┣⪼ Ⲃⲟⲧ Ⳳⲉʀⲋⲓⲟⲛ - 1.2.0")   
+    sed.info("║┣⪼ Ⳙⲣⲧⲓⲙⲉ - 00h:00m:4s ")
+    sed.info("║┣⪼ Ⲃⲟⲧ Ⲣⲓⲛⳋ - 0.006")
+    sed.info("║┣⪼ Ⲣⲩⲧⲏⲟⲛ - 3.9.2")
+    sed.info("║┣⪼ Ⲧⲉⳑⲉⲧⲏⲟⲛ - 1.17.0 ")
+    sed.info("║┣⪼ ✨Đ₳Ɽ₭ Ƒմʂʂìօղ✨")
+    sed.info("║╰━━━━━━━━━━━━━━━➣ ")
+    sed.info("╚══════════════════❍⊱❁۪۪")
+else:
+    sed.info("Đ₳Ɽ₭ Ƒմʂʂìօղ Has Been Installed Sucessfully !")
+    sed.info("You Can Visit @Dark_Fussion_chat For Any Support Or Doubts")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
