e84114f8c2c0072c0c76c1c911031b1423c5724f
Red warp <88662082+TeamFussion@users.noreply.github.com>
2021-11-22 14:16:24 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,108 +1,106 @@
-import glob
-import logging
-import os
-from pathlib import Path
+from userbot import bot
 from sys import argv
-
-import telethon.utils
+import sys
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+import os
 from telethon import TelegramClient
-
-from userbot import CMD_HNDLR, bot
-from userbot.Config import Var
-from userbot.thunderconfig import Config
-from userbot.utils import load_assistant, load_module, start_assistant
+from var import Var
+from userbot.Config import Config
 from telethon.tl.functions.channels import InviteToChannelRequest, JoinChannelRequest
+from userbot.utils import load_module
+from userbot import LOAD_PLUG, LOGS, JARVISversion
+from pathlib import Path
+import asyncio
+import glob
+import telethon.utils
 
-TELE = Var.PRIVATE_GROUP_ID
-BOTNAME = Var.TG_BOT_USER_NAME_BF_HER
-LOAD_MYBOT = Var.LOAD_MYBOT
-sed = logging.getLogger("Dark Fussion")
-fusion_pic = "https://te.legra.ph/file/56615a80e56dcca9dcfa0.jpg"
+JARVIS_PIC ="https://te.legra.ph/file/7aa60202b95b798a2a4bb.jpg"
 
 async def add_bot(bot_token):
-    await bot.start(bot_token)
-    bot.me = await bot.get_me()
-    bot.uid = telethon.utils.get_peer_id(bot.me)
-"""
-
-async def startup_log_all_done():
     try:
-        await bot.send_message(
-            TELE,
-            f"Đ₳Ɽ₭ Ƒմʂʂìօղ has been deployed.\nSend {CMD_HNDLR}alive to see if the bot is working.\n\nAdd @{BOTNAME} to this group and make it admin for enabling all the features of userbot",
-        )
-    except BaseException:
-        print("Either PRIVATE_GROUP_ID is wrong or you have left the group.")
-
-
-if len(argv) not in (1, 3, 4):
+        await bot.start(bot_token)
+        bot.me = await bot.get_me()
+        bot.uid = telethon.utils.get_peer_id(bot.me)
+    except Exception as e:
+        print(f"JARVIS_STRING - {str(e)}")
+        sys.exit()
+        
+        
+if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
     bot.tgbot = None
     if Var.TG_BOT_USER_NAME_BF_HER is not None:
         print("Initiating Inline Bot")
-        # ForTheGreatrerGood of beautification
         bot.tgbot = TelegramClient(
-            "TG_BOT_TOKEN", api_id=Var.APP_ID, api_hash=Var.API_HASH
-        ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
-        print("Initialisation finished, no errors")
+            "TG_BOT_TOKEN_BF_HER",
+            api_id=Var.APP_ID,
+            api_hash=Var.API_HASH
+        ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER
+        print("Initialisation finished with no errors")
         print("Starting Userbot")
         bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
         print("Startup Completed")
     else:
         bot.start()
-"""
+print("Loading Modules / Plugins")
 
-path = "userbot/plugins/*.py"
-files = glob.glob(path)
-for name in files:
+async def main_pl():
+  import glob
+  path = 'userbot/*.py'
+  files = glob.glob(path)
+  for name in files:
     with open(name) as f:
-        path1 = Path(f.name)
-        shortname = path1.stem
-        try:
-            
-         load_module(shortname.replace(".py", ""))
-        except Exception:
-            pass
-print("Đ₳Ɽ₭ Ƒմʂʂìօղ has been deployed! ")
-
-print("Setting up Đ₳Ɽ₭ Ƒմʂʂìօղ")
-
-
-if Config.ENABLE_ASSISTANTBOT == "ENABLE":
+      path1 = Path(f.name)
+      shortname = path1.stem
+      load_module(shortname.replace(".py", ""))
+"""
+async def assistant():
     path = "userbot/plugins/assistant/*.py"
     files = glob.glob(path)
     for name in files:
-        with open(name) as f:
-            path1 = Path(f.name)
-            shortname = path1.stem
-            try:
-                
-            
-             load_assistant(shortname.replace(".py", ""))
-            except Exception:
-                pass
-    sed.info("Đ₳Ɽ₭ Ƒմʂʂìօղ Has Been Deployed Successfully !")
-    sed.info("╔════❰ Ⲃⲟⲧ Ⲓⲛϝⲟʀⲙⲁⲧⲓⲟⲛ ❱═❍⊱❁۪۪")
-    sed.info("║┣⪼ Ⲟⲱⲛⲉʀ - Ƒմʂʂìօղ ᴜꜱᴇʀ ")
-    sed.info("║┣⪼ Ⲋⲧⲁⲧυⲋ - Ⲟⲛⳑⲓⲛⲉ")
-    sed.info("║┣⪼ Ⲃⲟⲧ Ⳳⲉʀⲋⲓⲟⲛ - 1.2.0")   
-    sed.info("║┣⪼ Ⳙⲣⲧⲓⲙⲉ - 00h:00m:4s ")
-    sed.info("║┣⪼ Ⲃⲟⲧ Ⲣⲓⲛⳋ - 0.006")
-    sed.info("║┣⪼ Ⲣⲩⲧⲏⲟⲛ - 3.9.2")
-    sed.info("║┣⪼ Ⲧⲉⳑⲉⲧⲏⲟⲛ - 1.17.0 ")
-    sed.info("║┣⪼ ✨Đ₳Ɽ₭ Ƒմʂʂìօղ✨")
-    sed.info("║╰━━━━━━━━━━━━━━━➣ ")
-    sed.info("╚══════════════════❍⊱❁۪۪")
-else:
-    sed.info("Đ₳Ɽ₭ Ƒմʂʂìօղ Has Been Installed Sucessfully !")
-    sed.info("You Can Visit @Dark_Fussion_chat For Any Support Or Doubts")
+      with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        start_assistant(shortname.replace(".py", ""))
+        extra_repo = "https://github.com/JARVIS-USERBOT/JARVIS-BOT"
+        try:
+            os.system(f"git clone {extra_repo}")  
+        except BaseException:
+            pass
+        import glob
+        LOGS.info("Loading Addons")
+        path = "JARVIS-DATA-BASE/*.py"
+        files = glob.glob(path)
+        for name in files:
+            with open(name) as ex:
+                path2 = Path(ex.name)
+                shortname = path2.stem
+                try:
+                    load_addons(shortname.replace(".py", ""))
+                    if not shortname.startswith("__") or shortname.startswith("_"):
+                        LOGS.info(f"[JARVIS-1.0] - Addons -  Installed - {shortname}")
+                except Exception as e:
+                    LOGS.warning(f"[JARVIS-1.0] - Addons - ERROR - {shortname}")
+                    LOGS.warning(str(e))
+    else:
+        print("Addons Not Loading")
+"""
+bot.loop.run_until_complete(main_pl())
+print(f"""
+ ________________
+|                    |
+|JARVIS-BOT TURNED ON|
+|____________________|""")
+
+
 
 async def jarvis_is_on():
     try:
-        if Config.COMBINED_GROUP_ID != 0:
+        if Config.LOGGER_ID != 0:
             await bot.send_file(
-                Config.COMBINED_GROUP_ID,
+                Config.LOGGER_ID,
+                JARVIS_PIC,
                 caption=f"DEPLOYED JARVIS BOT",
             )
     except Exception as e:
@@ -119,7 +117,9 @@ async def jarvis_is_on():
          pass
 
 
-if len(argv) not in (1, 3, 4):
+bot.loop.create_task(jarvis_is_on())
+
+if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
     bot.run_until_disconnected()
