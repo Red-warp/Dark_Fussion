93eaa4ecc224fcd96a363c6c75802a56615181ed
Red warp <88662082+TeamFussion@users.noreply.github.com>
2021-11-22 21:37:38 +0530

    Create __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -11,28 +11,30 @@ from userbot import CMD_HNDLR, bot
 from userbot.Config import Var
 from userbot.thunderconfig import Config
 from userbot.utils import load_assistant, load_module, start_assistant
+from telethon.tl.functions.channels import InviteToChannelRequest, JoinChannelRequest
+from telethon.tl import functions
 
 TELE = Var.PRIVATE_GROUP_ID
 BOTNAME = Var.TG_BOT_USER_NAME_BF_HER
 LOAD_MYBOT = Var.LOAD_MYBOT
-sed = logging.getLogger("Black Lightning")
-PYTHON_PIC = "https://te.legra.ph/file/56615a80e56dcca9dcfa0.jpg"
+sed = logging.getLogger("Dark Fussion")
+fusion_pic = "https://te.legra.ph/file/56615a80e56dcca9dcfa0.jpg"
 
 async def add_bot(bot_token):
     await bot.start(bot_token)
     bot.me = await bot.get_me()
     bot.uid = telethon.utils.get_peer_id(bot.me)
-"""
 
-#async def startup_log_all_done():
-    #try:
-      #  await bot.send_message(
-     #       TELE,
-      #      f"**Black Lightning has been deployed.\nSend** `{CMD_HNDLR}alive` **to see if the bot is working.\n\nAdd** @{BOTNAME} **to this group and make it admin for enabling all the features of userbot**",
-       )
+
+async def startup_log_all_done():
+    try:
+        await bot.send_message(
+            TELE,
+            f"Đ₳Ɽ₭ Ƒմʂʂìօղ has been deployed.\nSend {CMD_HNDLR}alive to see if the bot is working.\n\nAdd @{BOTNAME} to this group and make it admin for enabling all the features of userbot",
+        )
     except BaseException:
         print("Either PRIVATE_GROUP_ID is wrong or you have left the group.")
-"""
+
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
@@ -46,10 +48,10 @@ else:
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
         print("Initialisation finished, no errors")
         print("Starting Userbot")
-        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        bot.loop.run_until_complete(add_bot())
         print("Startup Completed")
     else:
-        bot.start()
+        bot.run_until_disconnected()
 
 path = "userbot/plugins/*.py"
 files = glob.glob(path)
@@ -62,9 +64,9 @@ for name in files:
          load_module(shortname.replace(".py", ""))
         except Exception:
             pass
-print("Lightning has been deployed! ")
+print("Đ₳Ɽ₭ Ƒմʂʂìօղ has been deployed! ")
 
-print("Setting up Lightning")
+print("Setting up Đ₳Ɽ₭ Ƒմʂʂìօղ")
 
 
 if Config.ENABLE_ASSISTANTBOT == "ENABLE":
@@ -80,52 +82,23 @@ if Config.ENABLE_ASSISTANTBOT == "ENABLE":
              load_assistant(shortname.replace(".py", ""))
             except Exception:
                 pass
-    sed.info("Black Lightning Has Been Deployed Successfully !")
+    sed.info("Đ₳Ɽ₭ Ƒմʂʂìօղ Has Been Deployed Successfully !")
     sed.info("╔════❰ Ⲃⲟⲧ Ⲓⲛϝⲟʀⲙⲁⲧⲓⲟⲛ ❱═❍⊱❁۪۪")
-    sed.info("║┣⪼ Ⲟⲱⲛⲉʀ - ʙʟᴀᴄᴋ-ʟɪɢʜᴛɴɪɴɢ ᴜꜱᴇʀ ")
+    sed.info("║┣⪼ Ⲟⲱⲛⲉʀ - Ƒմʂʂìօղ ᴜꜱᴇʀ ")
     sed.info("║┣⪼ Ⲋⲧⲁⲧυⲋ - Ⲟⲛⳑⲓⲛⲉ")
     sed.info("║┣⪼ Ⲃⲟⲧ Ⳳⲉʀⲋⲓⲟⲛ - 1.2.0")   
     sed.info("║┣⪼ Ⳙⲣⲧⲓⲙⲉ - 00h:00m:4s ")
     sed.info("║┣⪼ Ⲃⲟⲧ Ⲣⲓⲛⳋ - 0.006")
     sed.info("║┣⪼ Ⲣⲩⲧⲏⲟⲛ - 3.9.2")
     sed.info("║┣⪼ Ⲧⲉⳑⲉⲧⲏⲟⲛ - 1.17.0 ")
-    sed.info("║┣⪼ ✨𝐁𝐥𝐚𝐜𝐤-𝐋𝐢𝐠𝐡𝐭𝐧𝐢𝐧𝐠 𝐔𝐬𝐞𝐫𝐛𝐨𝐭✨")
+    sed.info("║┣⪼ ✨Đ₳Ɽ₭ Ƒմʂʂìօղ✨")
     sed.info("║╰━━━━━━━━━━━━━━━➣ ")
     sed.info("╚══════════════════❍⊱❁۪۪")
 else:
-    sed.info("Black Lightning Has Been Installed Sucessfully !")
-    sed.info("You Can Visit @lightning_support For Any Support Or Doubts")
+    sed.info("Đ₳Ɽ₭ Ƒմʂʂìօղ Has Been Installed Sucessfully !")
+    sed.info("You Can Visit @Dark_Fussion_chat For Any Support Or Doubts")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
-    bot.run_until_disconnected()               
-                
-                
-    async def lightning_is_on():
-    try:
-        if Config.PRIVATE_GROUP_ID != 0:
-            await bot.send_file(
-                Config.PRIVATE_GROUP_ID,
-                PYTHON_PIC,
-                caption=f"#START \n\nDeployed PYTHONBOT Successfully",
-            )
-    except Exception as e:
-        print(str(e))
-
-# Join PythonBot Channel after deploying 🤐😅
-  
-
-
-bot.loop.create_task(lightning_is_on())
-
-if len(sys.argv) not in (1, 3, 4):
-    bot.disconnect()
-else:
-    bot.run_until_disconnected()           
-                
-                
-                
-               
-                
-                
+    bot.run_until_disconnected()
