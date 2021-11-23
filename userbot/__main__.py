73c0b9fee522c415d6677343bbfdf1e30e28d2d6
Red warp <88662082+TeamFussion@users.noreply.github.com>
2021-11-23 22:14:27 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -44,7 +44,7 @@ else:
         print("Initiating Inline Bot")
         # ForTheGreatrerGood of beautification
         bot.tgbot = TelegramClient(
-            "TG_BOT_TOKEN", api_id=Var.APP_ID, api_hash=Var.API_HASH
+            "TG_BOT_TOKEN_BF_HER", api_id=Var.APP_ID, api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
         print("Initialisation finished, no errors")
         print("Starting Userbot")
